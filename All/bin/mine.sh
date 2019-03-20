2d906f73f611970d98c785277e76e7ca38fefa1c
Matthew Pfeiffer <spferical@gmail.com>
2019-03-20 01:13:02 -0400

    Delete old scripts

--- All/bin/mine.sh
+++
@@ -1,5 +0,0 @@
-xfce4-terminal &#-x ~/cgminescript.sh &
-#xfce4-terminal -x htop &
-xfce4-terminal -x watch aticonfig --pplib-cmd \"get fanspeed 0\" &
-xfce4-terminal -x watch sensors &
-xfce4-terminal -x watch aticonfig --odgt &
