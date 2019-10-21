e7f260faae1a2e0ec5c36fb61d52956fb693e588
Matthew Pfeiffer <spferical@gmail.com>
2019-10-21 00:03:55 -0400

    Format backup script

--- All/bin/backupbalrog.sh
+++ All/bin/backupbalrog.sh
@@ -1,1 +1,20 @@
-borg create -v --list -s --compression lzma -e $HOME/.cache -e "__pycache__" -e "*.pyc" -e $HOME/.local/share/Steam/ -e $HOME/Games/ -e $HOME/Downloads -e $HOME/.PyCharm2016.1 -e $HOME/.tor-browser-en -e $HOME/.wine -e $HOME/enc -e $HOME/quote "$1:/media/backup/balrog::balrog-{now}" $HOME/
+backup_date=${2:-\{now\}}
+echo $backup_date
+borg create -v\
+    --list -s\
+    --compression lzma\
+    -e $HOME/.cache\
+    -e "__pycache__"\
+    -e "*.pyc"\
+    -e $HOME/.local/share/Steam/\
+    -e $HOME/Games/\
+    -e $HOME/Downloads\
+    -e $HOME/.PyCharm2016.1\
+    -e $HOME/.tor-browser-en\
+    -e $HOME/.wine\
+    -e $HOME/enc\
+    -e $HOME/quote\
+    -e $HOME/.rustup\
+    -e $HOME/.cargo\
+    "$1:/media/backup/balrog::balrog-$backup_date"\
+    $HOME/
