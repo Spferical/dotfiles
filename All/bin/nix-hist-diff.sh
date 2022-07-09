69f3ac0ba0825b2077f012c657e78d96e77e1d6a
Matthew Pfeiffer <spferical@gmail.com>
2022-07-09 19:53:48 -0400

    nix-hist-diff.sh: handle ctrl+c better

--- All/bin/nix-hist-diff.sh
+++ All/bin/nix-hist-diff.sh
@@ -4,7 +4,13 @@
 diff() {
     ns=($(find /nix/var/nix/profiles/system-*-link | grep -Eo '[0-9]+' | sort -g -r))
     len=${#ns[@]}
+    interrupted=0
+    on_interrupt() {
+        interrupted=1
+    }
+    trap on_interrupt SIGINT
     for i in $(seq 0 $((len-2))); do
+        [[ interrupted -ne 0 ]] && { echo "Ctrl-C caught, exiting..." ; exit ; }
         nvd --color=always diff \
             /nix/var/nix/profiles/system-{"${ns[$((i+1))]}","${ns[$i]}"}-link
     done
