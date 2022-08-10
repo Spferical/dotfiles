8e648c952f59a92c5e438db51d9f9a22be393e68
Matthew Pfeiffer <spferical@gmail.com>
2022-08-09 20:22:40 -0400

    low-bat-notify.sh: don't assume battery index
    Dunno if it's suspend or other driver shenanigans, but my sole battery
    sometimes shows up as BAT0 and sometimes as BAT1.

--- All/bin/low-bat-notify.sh
+++ All/bin/low-bat-notify.sh
@@ -1,19 +1,9 @@
 #!/usr/bin/env bash
 
-# low battery in %
-LOW_BATTERY="10"
-# path to battery /sys
-BATTERY_PATH="/sys/class/power_supply/BAT0/"
-
 while true; do
-	if [ -e "$BATTERY_PATH" ]; then
-	    BATTERY_ON=$(cat $BATTERY_PATH/status)
-	    if [ "$BATTERY_ON" != "Charging" ]; then
-		CURRENT_BATTERY=$(cat $BATTERY_PATH/capacity)
-		if [ "$CURRENT_BATTERY" -lt "$LOW_BATTERY" ]; then
-		    notify-send -u critical  "Battery is low! - $CURRENT_BATTERY%"
-		fi
-	    fi
-	fi
-	sleep 60
+    percent=$(acpi -b | head -n 1 | grep -P -o '[0-9]+(?=%)')
+    if [ $percent -le 10 ]; then
+        notify-send -w -u critical  "Battery is low! - ${percent}%"
+    fi
+    sleep 60
 done
