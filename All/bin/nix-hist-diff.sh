c21068ce8f21115f56d54fe0550246e224b80c27
Matthew Pfeiffer <spferical@gmail.com>
2022-08-09 20:25:48 -0400

    nix-hist-diff.sh: add dates

--- All/bin/nix-hist-diff.sh
+++ All/bin/nix-hist-diff.sh
@@ -11,8 +11,10 @@ diff() {
     trap on_interrupt SIGINT
     for i in $(seq 0 $((len-2))); do
         [[ interrupted -ne 0 ]] && { echo "Ctrl-C caught, exiting..." ; exit ; }
-        nvd --color=always diff \
-            /nix/var/nix/profiles/system-{"${ns[$((i+1))]}","${ns[$i]}"}-link
+        local new="/nix/var/nix/profiles/system-${ns[$((i+1))]}-link"
+        local old="/nix/var/nix/profiles/system-${ns[$i]}-link"
+        echo "${new} built $(stat -c %y $new)"
+        nvd --color=always diff "${new}" "${old}"
     done
 }
 
