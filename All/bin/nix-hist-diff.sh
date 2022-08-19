e314a83fe6112e90e4ccc50105716ce5f27bde1f
Matthew Pfeiffer <spferical@gmail.com>
2022-08-18 20:19:50 -0400

    nix-hist-diff.sh: fix date off-by-one

--- All/bin/nix-hist-diff.sh
+++ All/bin/nix-hist-diff.sh
@@ -2,19 +2,22 @@
 # Like 'git log' but for nix system configurations.
 
 diff() {
+    local ns
     ns=($(echo /nix/var/nix/profiles/system-*-link | grep -Eo '[0-9]+' | sort -g -r))
-    len=${#ns[@]}
-    interrupted=0
+    local len=${#ns[@]}
+    local interrupted=0
     on_interrupt() {
         interrupted=1
     }
     trap on_interrupt SIGINT
     for i in $(seq 0 $((len-2))); do
         [[ interrupted -ne 0 ]] && { echo "Ctrl-C caught, exiting..." ; exit ; }
-        local new="/nix/var/nix/profiles/system-${ns[$((i+1))]}-link"
-        local old="/nix/var/nix/profiles/system-${ns[$i]}-link"
-        echo "${new} built $(stat -c %y $new)"
-        nvd --color=always diff "${new}" "${old}"
+        local old="/nix/var/nix/profiles/system-${ns[$((i+1))]}-link"
+        local new="/nix/var/nix/profiles/system-${ns[$i]}-link"
+        local newdate
+        newdate=$(stat -c %y "${new}")
+        echo "${new} built ${newdate})"
+        nvd --color=always diff "${old}" "${new}"
     done
 }
 
