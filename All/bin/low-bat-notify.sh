71431eda18cf2d792a37cb5dd8d429dcd84b7baf
Matthew Pfeiffer <spferical@gmail.com>
2022-08-18 20:20:49 -0400

    low-bat-notify.sh: work with multiple detected batteries

--- All/bin/low-bat-notify.sh
+++ All/bin/low-bat-notify.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 while true; do
-    percent=$(acpi -b | head -n 1 | grep -P -o '[0-9]+(?=%)')
-    if [ $percent -le 10 ]; then
+    percent=$(acpi -b | grep -P -o '[0-9]+(?=%)' | sort -n --reverse | head -n 1)
+    if [ "${percent}" -le 10 ]; then
         notify-send -w -u critical  "Battery is low! - ${percent}%"
     fi
     sleep 60
