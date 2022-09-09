d921ce6b61f8b35e33c211bf5e56530eb1e160a9
Matthew Pfeiffer <spferical@gmail.com>
2022-09-09 02:22:51 -0400

    low-bat-notify.sh: detect charging

--- All/bin/low-bat-notify.sh
+++ All/bin/low-bat-notify.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/env bash
 
 while true; do
-    percent=$(acpi -b | grep -P -o '[0-9]+(?=%)' | sort -n --reverse | head -n 1)
-    if [ "${percent}" -le 10 ]; then
-        notify-send -w -u critical  "Battery is low! - ${percent}%"
+    charging=0
+    max_charge=0
+    # There may be extra dead batteries or zombie batteries left over by a
+    # suspend/resume. So, consider any charging or fullish battery ok.
+    while read -r line; do
+        [[ ${line} =~ ^Battery\ [0-9]+:\ ([a-zA-Z\ ]+),\ ([0-9]+)% ]]
+        charge="${BASH_REMATCH[2]}"
+        ((charge>max_charge)) && max_charge=${charge}
+        if [[ "Charging" == "${BASH_REMATCH[1]}" ]]; then
+            charging=1
+        fi
+    done < <(acpi -b)
+    if [[ ${charging} == 0 && "${max_charge}" -le 10 ]]; then
+        notify-send -w -u critical "Battery is low! - ${max_charge}%"
     fi
     sleep 60
 done
